a6b15c75127d22af077fea1aff062060e0e70805
Priyanshu Raj <rishuraj718@gmail.com>
2023-04-23 21:05:17 +0530

    Add files via upload

---
+++ extra.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+if [ ! -x "$(command -v pip3)" ];
+then
+	echo "[!] It looks you don't have pip3..looking for pip installation.."
+	exit 1
+elif [ ! -x "$(command -v pip)" ];
+then
+	echo "[!] You must need pip respective of your python version to run this script.."
+	exit 1
+else
+	echo "running installation.."
+fi
+
+if [ $? -eq 0 ];
+then
+	pip3 install -r recce/requirements.txt
+else
+	pip install -r recce/requirements.txt
+fi
+
+if [ $? -eq 0 ];
+then
+	echo "creating a command for you.."
+	sudo cp recce/recce.py /usr/local/bin/recce >&2
+fi
+if [ $? -eq 0 ];
+then
+	echo "Done"
+else
+	echo "Some error occured"
+fi
+
