ec6348f7ce056a9ad20f6898a28dfb87b5f54045
Priyanshu Raj <rishuraj718@gmail.com>
2023-04-23 21:00:42 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# create bug folder
+mkdir ~/bug
+
+#give execution permission  sub4bug
+chmod +x sub4bug
+
+#copy sub4bug  bin
+sudo cp sub4bug /bin/sub4bug
+
+
+# install asset-finder
+go install github.com/mnomnom/assetfinder@latest
+# install sub-finder
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
+
+# install Recce
+git clone https://github.com/unstabl3/recce.git
+mv extra/install.sh recce/install.sh
+chmod +x recce/recce.py
+bash recce/install.sh
+
+# install Takeover
+git clone https://github.com/m4ll0k/takeover.git
+python3 takeover/setup.py install
